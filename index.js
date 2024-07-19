873a023dd7163fe209d8942798dbb5a11811d428
sumit-linearloop <sumit@linearloop.io>
2024-07-19 13:03:20 +0530

    update index.js file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello DevOps!');
+    res.send('Hello DevOps I am working DevOps Engineer!');
 });
 
 app.listen(port, () => {
