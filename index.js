8b5141156c40a935ff43c40d447f478596342a87
sumit-linearloop <sumit@linearloop.io>
2024-07-22 19:20:05 +0530

    f

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello DevOps AWS docker linux jenkins/ci-cd!');
+    res.send('Hello DevOps AWS docker linux jenkins!');
 });
 
 app.listen(port, () => {
