82c1f5dc87daeb10e6d3640b0f67c816f3689ea2
sumit-linearloop <sumit@linearloop.io>
2024-07-24 17:42:36 +0530

    github remove

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello DevOps ci-cd pipeline with github action!');
+    res.send('Hello DevOps ci-cd pipeline!');
 });
 
 app.listen(port, () => {
