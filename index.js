055c1e97840d4d16b2a6fbabbcdf8229097f1ac2
sumit-linearloop <sumit@linearloop.io>
2024-07-24 17:10:21 +0530

    ci-cd pipeline update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello DevOps!');
+    res.send('Hello DevOps ci-cd pipeline!');
 });
 
 app.listen(port, () => {
