f8554e540fdd5a1b094e39726832a16967d09124
sumit-linearloop <sumit@linearloop.io>
2024-07-25 10:50:29 +0530

    github

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello DevOps ci-cd pipeline create and push kubernetes!');
+    res.send('Hello DevOps ci-cd pipeline create and push kubernetes with github action!');
 });
 
 app.listen(port, () => {
