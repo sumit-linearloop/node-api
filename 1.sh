7e2eb1f4ec63bd67ea19e0a9f0bfea071bae2a6f
sumit-linearloop <sumit@linearloop.io>
2024-08-12 14:50:51 +0530

    none

--- 1.sh
+++ 1.sh
@@ -1,25 +1,25 @@
+# # #!/bin/bash
+
+# # SECRET_NAME="Envfile"  
+# # REGION="us-east-1" 
+
+# # SECRET=$(aws secretsmanager get-secret-value --secret-id Envfile --region us-east-1 --query SecretString --output text)
+
+# # if [ $? -ne 0 ]; then
+# #     echo "Error fetching secret"
+# #     exit 1
+# # fi
+
+# # echo -n "" > .env
+
+# # echo "${SECRET}" | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> .env
+
+# # while IFS= read -r line; do
+# #     export "$line"
+# # done < .env
+
+
 # #!/bin/bash
 
-# SECRET_NAME="Envfile"  
-# REGION="us-east-1" 
-
-# SECRET=$(aws secretsmanager get-secret-value --secret-id Envfile --region us-east-1 --query SecretString --output text)
-
-# if [ $? -ne 0 ]; then
-#     echo "Error fetching secret"
-#     exit 1
-# fi
-
-# echo -n "" > .env
-
-# echo "${SECRET}" | jq -r 'to_entries[] | "\(.key)=\(.value)"' >> .env
-
-# while IFS= read -r line; do
-#     export "$line"
-# done < .env
-
-
-#!/bin/bash
-
-# Fetch secrets from AWS Secrets Manager
-aws secretsmanager get-secret-value --secret-id Envfile --query SecretString --output text > /app/.env
+# # Fetch secrets from AWS Secrets Manager
+# aws secretsmanager get-secret-value --secret-id Envfile --query SecretString --output text > /app/.env
